f82254911bdbdefa788143366e0016f2fb9cab8f
Pablo Lunarenko <kimiaropablo958@gmail.com>
2013-06-13 18:43:09 -0300

    ups

--- janice.mk
+++ janice.mk
@@ -192,7 +192,7 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    persist.sys.usb.config=mtp,adb
+    persist.sys.usb.config=mtp
 
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
