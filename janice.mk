d50812b86ae6eb593fe7782d10366693a64cb1cc
Pablo Lunarenko <kimiaropablo958@gmail.com>
2013-06-16 20:54:30 -0300

    This needs to be as this for some reason, lol

--- janice.mk
+++ janice.mk
@@ -117,7 +117,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=Samsungu8500RIL \
+    ro.telephony.ril_class=SamsungU8500RIL \
     mobiledata.interfaces=pdp0,wlan0,gprs,ppp0 \
     ro.ril.hsxpa=1 \
     dev.sfbootcomplete=0 \
